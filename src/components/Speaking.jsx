14b528fbab2be76d29e99442055074c24fabb2bd
Varnikumar Patel <varnikumar1512@gmail.com>
2025-05-26 15:40:32 +0530

    Update Speaking.jsx

--- src/components/Speaking.jsx
+++ src/components/Speaking.jsx
@@ -40,7 +40,7 @@ const SpeakingPage = () => {
                   to="/contact"
                   className="bg-[#f39c12] text-white px-6 py-2 rounded-sm hover:bg-[#e67e22] transition duration-300"
                 >
-                Contact Us
+                Contact Now
                 </Link>
               </div>
             </div>
