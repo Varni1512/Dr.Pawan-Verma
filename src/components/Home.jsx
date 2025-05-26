d1e052b60d176941024e0d2039e17dba8407370e
Varnikumar Patel <varnikumar1512@gmail.com>
2025-05-26 15:39:56 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,47 +1,54 @@
-import React from 'react'
-import Hero from '../components/NewHome/Hero'
-import FictionBookPage from '../components/NewHome/FictionBookPage'
-import NonFictionBookPage from '../components/NewHome/NonFictionBookPage'
-import BookPublished from '../components/NewHome/BookPublished'
-import BookAtGlance from '../components/NewHome/BookAtGlance'
-import EventsAndActivities from '../components/NewHome/EventsAndActivities'
-import ProfessionalSpeaker from '../components/NewHome/ProfessionalSpeaker'
-import ManagementConsultant from '../components/NewHome/ManagementConsultant'
-import HeroSection from '../components/NewHome/HeroSection'
-import AwardsSection from '../components/NewHome/AwardsSection'
-import BlogsSection from '../components/NewHome/BlogsSection'
-import TestimonialsSection from '../components/NewHome/TestimonialsSection'
-import SocialMediaSection from '../components/NewHome/SocialMediaSection'
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Hero from '../components/NewHome/Hero';
+import FictionBookPage from '../components/NewHome/FictionBookPage';
+import NonFictionBookPage from '../components/NewHome/NonFictionBookPage';
+import BookPublished from '../components/NewHome/BookPublished';
+import BookAtGlance from '../components/NewHome/BookAtGlance';
+import EventsAndActivities from '../components/NewHome/EventsAndActivities';
+import ProfessionalSpeaker from '../components/NewHome/ProfessionalSpeaker';
+import ManagementConsultant from '../components/NewHome/ManagementConsultant';
+import HeroSection from '../components/NewHome/HeroSection';
+import AwardsSection from '../components/NewHome/AwardsSection';
+import BlogsSection from '../components/NewHome/BlogsSection';
+import TestimonialsSection from '../components/NewHome/TestimonialsSection';
+import SocialMediaSection from '../components/NewHome/SocialMediaSection';
 
 export default function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const target = document.querySelector(location.hash);
+      if (target) {
+        // Delay to ensure DOM is rendered before scrolling
+        setTimeout(() => {
+          target.scrollIntoView({ behavior: 'smooth' });
+        }, 100);
+      }
+    }
+  }, [location]);
+
   return (
-    // <div>
-    //   <Hero />
-    //   {/* <PowerfulWords/> */}
-    //   <Gallery />
-    //   <BookAtGlance />
-    //   <VideoTestimonials/>
-    //   <Videos />
-    //   <Blogs />
-    //   <Testimonials/>
-    // </div>
     <div>
       <Hero />
-      <section id="non-fiction">
+      
+      <section id="nonfiction">
         <NonFictionBookPage />
       </section>
 
       <FictionBookPage />
       <BookAtGlance />
       <BookPublished />
-      <EventsAndActivities />
+      
       <ProfessionalSpeaker />
       <ManagementConsultant />
       <HeroSection />
       <AwardsSection />
+      <EventsAndActivities />
       <BlogsSection />
       <TestimonialsSection />
       <SocialMediaSection />
     </div>
-  )
+  );
 }
