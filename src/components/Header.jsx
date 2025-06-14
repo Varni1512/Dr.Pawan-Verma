ebdf8561196babe4e0c2425899f50d6db409cf95
Varnikumar Patel <varnikumar1512@gmail.com>
2025-06-14 13:14:31 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-black text-white text-[24px]">
+    <header className="bg-black text-white text-[20px]">
       <div className="container mx-auto px-4 py-3 flex flex-wrap justify-between items-center">
         {/* Logo */}
         <Link to='/' className="flex items-center mr-4">
@@ -70,7 +70,7 @@ const Header = () => {
               <a
                 key={link.href}
                 href={link.href}
-                className={`relative px-2 lg:px-3 py-2 transition duration-300 text-[28px] ${isActive ? "text-yellow-500" : "hover:text-yellow-500"}`}
+                className={`relative px-2 lg:px-3 py-2 transition duration-300 text-[20px] ${isActive ? "text-yellow-500" : "hover:text-yellow-500"}`}
               >
                 {link.label}
                 {isActive && <div className="absolute bottom-0 left-0 w-full h-1 bg-yellow-500"></div>}
@@ -82,7 +82,7 @@ const Header = () => {
           <div className="relative" onClick={(e) => e.stopPropagation()}>
             <button
               onClick={toggleMoreDropdown}
-              className={`flex items-center px-2 lg:px-3 py-2 transition duration-300 text-[28px] cursor-pointer ${isMoreItemActive ? "text-yellow-500" : "hover:text-yellow-500"}`}
+              className={`flex items-center px-2 lg:px-3 py-2 transition duration-300 text-[20px] cursor-pointer ${isMoreItemActive ? "text-yellow-500" : "hover:text-yellow-500"}`}
             >
               Media kit <ChevronDown size={16} className="ml-1" />
               {isMoreItemActive && <div className="absolute bottom-0 left-0 w-full h-1 bg-yellow-500"></div>}
@@ -96,7 +96,7 @@ const Header = () => {
                     <a
                       key={link.href}
                       href={link.href}
-                      className={`block px-4 py-2 transition duration-300 text-[28px] ${isActive ? "text-yellow-500" : "hover:text-yellow-500 hover:bg-gray-900"}`}
+                      className={`block px-4 py-2 transition duration-300 text-[20px] ${isActive ? "text-yellow-500" : "hover:text-yellow-500 hover:bg-gray-900"}`}
                     >
                       {link.label}
                     </a>
@@ -117,7 +117,7 @@ const Header = () => {
               <a
                 key={link.href}
                 href={link.href}
-                className={`relative block px-4 py-2 transition duration-300 text-[28px] ${isActive ? "text-yellow-500" : "hover:text-yellow-500"}`}
+                className={`relative block px-4 py-2 transition duration-300 text-[20px] ${isActive ? "text-yellow-500" : "hover:text-yellow-500"}`}
               >
                 {link.label}
                 {isActive && (
